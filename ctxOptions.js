162de8f09f41eec52057cd21bc9772e0eda8cd61
mbeckdev <mbeckdev42@gmail.com>
2022-02-01 19:08:29 -0700

    Take out test code

--- ctxOptions.js
+++ ctxOptions.js
@@ -5,7 +5,3 @@ let lineCapOptions = ['round', 'butt', 'square'];
 let ctxOptions = { lineCapOptions, lineJoinOptions };
 
 export { ctxOptions };
-
-//second.js
-let someWord = 'candy';
-export { someWord };
